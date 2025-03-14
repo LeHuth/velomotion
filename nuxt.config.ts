e23f818a04b575d783e44a18d842d7b3cd53f4f3
Leonard Huth <leonard.huth@googlemail.com>
2025-03-14 23:44:57 +0100

    upgraded to nuxtui pro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui','nuxt-mongoose'],
+  modules: ['@nuxt/ui-pro','nuxt-mongoose'],
   ui:{
     global:true,
     notifications: {
